c4a01342749636148c8e422edbda1814f4cebbab
知了凉亭轻声叫 <foryli@163.com>
2022-06-06 13:06:37 +0800

    feat: 项目核心搭建步骤

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>演示 Element Plus 基础组件</h1>
+
+  <h2>Button 按钮</h2>
+
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+  <el-button>中文</el-button>
+
+  <h2>Icon 图标</h2>
+
+  <el-icon :size="20">
+    <i-ep-edit />
+  </el-icon>
+  <el-icon class="no-inherit" color="#409EFC">
+    <i-ep-share />
+  </el-icon>
+  <el-icon>
+    <i-ep-delete />
+  </el-icon>
+  <el-icon class="is-loading">
+    <i-ep-loading />
+  </el-icon>
+  <el-button type="primary">
+    <el-icon style="vertical-align: middle">
+      <i-ep-search />
+    </el-icon>
+    <span style="vertical-align: middle"> Search </span>
+  </el-button>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+const Icons = require("unplugin-icons/webpack");
+const IconsResolver = require("unplugin-icons/resolver");
+const path = require("path");
+const pathSrc = path.resolve(__dirname, "src");
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  configureWebpack: (config) => {
+    config.plugins.push(
+      ...[
+        AutoImport({
+          resolvers: [ElementPlusResolver()],
+          dts: path.resolve(pathSrc, "auto-imports.d.ts")
+        }),
+        Components({
+          resolvers: [ElementPlusResolver(), IconsResolver()],
+          dts: path.resolve(pathSrc, "components.d.ts")
+        }),
+        Icons({
+          autoInstall: true
+        })
+      ]
+    );
+  }
 });
